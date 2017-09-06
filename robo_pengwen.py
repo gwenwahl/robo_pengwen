16533c462b555008dda84ee89cbac4e0ca91b2f7
Gwen Wahl <wahl.gwen@gmail.com>
2017-09-06 01:20:07 -0700

    Adding bot code

---
+++ robo_pengwen.py
@@ -0,0 +1,37 @@
+import discord
+import asyncio
+import sqlite3
+from datetime import datetime, timedelta
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!jar'):
+        if len(message.mentions) > 0: 
+            conn = sqlite3.connect('database.db')
+            c = conn.cursor()
+            report = (message.author.id,
+                      message.mentions[0].id,
+                      message.timestamp)
+            c.execute("INSERT INTO jar_reports ('author_id', 'report_id', 'reported_at') VALUES (?,?,?)", report)
+            conn.commit()
+            conn.close()
+            await client.send_message(message.channel, "You've been naughty {}!".format(message.mentions[0].nick))
+            await asyncio.sleep(2)
+            await client.delete_message(message)
+        else:
+            # Helpful error message    
+            err = await client.send_message(message.channel, 'You need to mention someone for this to work!: @Someone.')
+            await asyncio.sleep(11)
+            await client.delete_message(err)    
+
+client.run('im secret ;)')
---
+++ schema.sql
@@ -0,0 +1,6 @@
+CREATE TABLE jar_reports (
+	id INTEGER PRIMARY KEY AUTOINCREMENT,
+	author_id VARCHAR(256),
+	report_id VARCHAR(256),
+	reported_at DATETIME
+)
